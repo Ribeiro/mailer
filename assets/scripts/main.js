969838282871b6e0ae21ecfa27ca23d3bdd47e60
Geovanny Ribeiro <geovanny.ribeiro@gmail.com>
2015-12-15 09:13:02 -0300

    refactoring and tests

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,11 +3,27 @@
 
   var Mailer = function(config){
     var assertNotMissing = function(attributes){
+      var mandatoryProperties = ['service', 'password', 'user'];
+        var errors = [];
         if(!attributes || Object.getOwnPropertyNames(attributes).length === 0){
-          throw new Error('Please, provide configuration');
+          throw new Error('Please, provide configuration!');
         }
 
-        //Should add validator component here
+        var mandatoryPropertiesSize = mandatoryProperties.length;
+        var counter = 0;
+
+        while(counter < mandatoryPropertiesSize){
+          if (!attributes.hasOwnProperty(mandatoryProperties[counter])) {
+             errors.push('Property ' + mandatoryProperties[counter] +' is missing!');
+          }
+
+          counter++;
+        }
+
+        if(errors.length > 0){
+          throw new Error(errors.join(';'));
+        }
+
     };
 
     assertNotMissing(config);
--- test/test.js
+++ test/test.js
@@ -7,39 +7,35 @@ describe("Mailer", function(){
 
   describe("Constructor Success", function(){
     it("Should successfully instantiate Mailer", function(){
-        var config = {service: 'hotmail'};
+        var config = {service: 'hotmail', user: 'email@email.com', password:'password'};
         var mailer = new Mailer(config);
         assert.equal('hotmail', mailer.getServiceName());
     });
-
 
   });
 
   describe("Constructor Failure", function(){
     it("Should fail instantiating Mailer due to null config", function(){
         var config = null;
-        expect(function() { new Mailer(config); }).to.throw(Error);
+        expect(function() { new Mailer(config); }).to.throw(Error).and.have.property('message','Please, provide configuration!');
+
     });
 
     it("Should fail instantiating Mailer due to undefined config", function(){
         var config;
-        expect(function() { new Mailer(config); }).to.throw(Error);
+        expect(function() { new Mailer(config); }).to.throw(Error).and.have.property('message','Please, provide configuration!');
     });
 
     it("Should fail instantiating Mailer due to empty config", function(){
         var config = {};
-        expect(function() { new Mailer(config); }).to.throw(Error);
+        expect(function() { new Mailer(config); }).to.throw(Error).and.have.property('message','Please, provide configuration!');
     });
 
+    it("Should fail instantiating Mailer due to missing mandatory properties", function(){
+        var config = {service: 'hotmail'};
+        expect(function() { new Mailer(config); }).to.throw(Error).and.have.property('message','Property password is missing!;Property user is missing!');
+    });
 
   });
-
-  /*describe("#listArticles", function(){
-    it("should list articles", function(){
-      var author = new Author("Jim");
-      author.writeArticle("a great article");
-      assert.equal("Jim has written: a great article", author.listArticles());
-    });
-  });*/
 
 });
